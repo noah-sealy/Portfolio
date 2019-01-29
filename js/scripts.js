630d7d33d4cce53d9bdb42260673e887f045a389
noah-sealy <noahsealy@hotmail.com>
2019-01-29 19:21:40 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -110,7 +110,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["Landlord.", "Volunteer.", "Father.", "Jamie Sealy."],    /* You can change the home section typing text from
+            strings: ["Computer Science Intern.", "Dalhousie Student.", "Noah Sealy."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 60
           });
